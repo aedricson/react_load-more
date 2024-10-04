8588b89d82dda777720a9867de28d98bcf36a5c3
Ihor Shynkar <aedricson@gmail.com>
2024-10-04 10:29:27 +0300

    added load more functionality

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Product as ProductType } from "../../types/Product";
 
-export const Product: React.FC = () => {
+import './Product.css';
+
+type Props = {
+  product: ProductType;
+};
+
+export const Product: React.FC<Props> = ({ product }) => {
   return (
-    <></>
+    <div className="product">
+      <img
+        className="image"
+        src={product.thumbnail}
+        alt={product.title}
+      />
+      <h3 className="title">{product.title}</h3>
+      <div className="description">{product.description}</div>
+    </div>
   );
-}
+};
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,23 +1,62 @@
 import React, { useEffect, useState } from "react";
 
-import { getProducts } from '../../api/ProductsAPI';
+import { getProducts } from "../../api/ProductsAPI";
+import { Product } from "../product/Product";
+import { Product as ProductType } from "../../types/Product";
+import "./Products.css";
 
 export const Products: React.FC = () => {
-  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [currentSkipCount, setCurrentSkipCount] = useState<number>(0);
+  const [products, setProducts] = useState<ProductType[] | null>(null);
+
+  const handleIncrementSkipCount = () => {
+    setCurrentSkipCount((prevSkipCount) => prevSkipCount + 1);
+  };
 
   async function fetchData() {
-    const productsData = await getProducts(0);
-    setProducts(productsData)
+    try {
+      setIsLoading(true);
+
+      const productsData = await getProducts(currentSkipCount);
+      const mergedProducts =
+        products && products.length
+          ? [...products, ...productsData.products]
+          : productsData.products;
+
+      setProducts(mergedProducts);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessage(`An error occured when loading products: ${error}`);
+    }
   }
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentSkipCount]);
 
-  console.log(products);
-  
+  if (errorMessage) {
+    return <div className="error">{errorMessage}</div>;
+  }
 
   return (
-    <></>
+    <section>
+      <div className="products">
+        {products &&
+          products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        {isLoading && <div className="loading">Products loading...</div>}
+      </div>
+
+      <div className="products-load">
+        {!isLoading && (
+          <button onClick={handleIncrementSkipCount} className="load-button">
+            Load more
+          </button>
+        )}
+      </div>
+    </section>
   );
-}
+};
